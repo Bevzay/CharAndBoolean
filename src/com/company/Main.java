0d0b9c98ba98d686fafc0e67c81e5e8b9a7ee99f
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-21 12:30:44 +1300

    Char and Unicode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
     }
 }
