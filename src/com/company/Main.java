a90758d08edd8c44dfe9f2718b29fae820b8c1ed
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-21 13:01:54 +1300

    Boolean

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,9 @@ public class Main {
         char myCopyrightChar = '\u00A9';
         System.out.println(myCopyrightChar);
 
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+
     }
 }
